c88f243a5f5f7171c61491e76997ebc7d86bf624
Jkelly <108213769+sjk0503@users.noreply.github.com>
2024-10-19 18:50:28 +0900

    Update get_ID.py

--- aws/get_ID.py
+++ aws/get_ID.py
@@ -26,29 +26,81 @@ def lambda_handler(event, context):
     body = json.loads(event['body'])
 
     botNum = body['botNum']
+    # case == 'all', 모든 정보 리턴
+    # case == 'id', thread, assistant id 리턴
+    # case == '{cmd}', {cmd} 레코드 리턴
+    case = body['case']
+
 
     try:
         # bot 테이블에서 bot_number 키의 값이 botNum인 레코드를 찾고
         # 해당 레코드의 thread_id와 assistant_id 값을 가져오는 부분
+        data = {}
         with conn.cursor() as cur:
-            cur.execute("SELECT thread_id, assistant_id FROM bot WHERE bot_number = %s", (botNum,))
-            result = cur.fetchone()
+            if case == 'all':
+                cur.execute("SELECT * FROM bot WHERE bot_number = %s", (botNum))
+                result = cur.fetchone()
+                logger.info(result)
 
-            if result:
-                thread_id, assistant_id = result
-                logger.info(f"id 가져오기 완료 + {thread_id}, {assistant_id}")
-            else:
-                logger.error("해당 bot_number에 대한 레코드가 없습니다.")
-                return {
-                    'statusCode': 404,
-                    'body': json.dumps('not found')
-                }
+                if result:
+                    logger.info("가져오기 완료")
+                    data = {
+                        'statusCode': 200,
+                        'thread_id': result[1],
+                        'assistant_id': result[2],
+                        'characterName': result[3],
+                        'selectedCategories': result[4],
+                        'shortDescription': result[5],
+                        'detailedDescription': result[6],
+                        'prompt': result[7],
+                        'isSearchingLatestInfo': result[8],
+                        'isUpdatingUserInfo': result[9],
+                        'characterImage': result[10]
+                    }
+                else:
+                    logger.error("레코드가 없습니다.")
+                    return {
+                        'statusCode': 404,
+                        'body': json.dumps('not found')
+                    }
 
-        return {
-            'statusCode': 200,
-            'thread_id': thread_id,
-            'assistant_id': assistant_id
-        }
+            elif case == 'id':
+                cur.execute("SELECT thread_id, assistant_id FROM bot WHERE bot_number = %s", (botNum))
+                result = cur.fetchone()
+                logger.info(result)
+
+                if result:
+                    thread_id, assistant_id = result
+                    logger.info("가져오기 완료")
+                    data = {
+                        'statusCode': 200,
+                        'thread_id': thread_id,
+                        'assistant_id': assistant_id
+                    }
+                else:
+                    logger.error("해당 bot_number에 대한 레코드가 없습니다.")
+                    return {
+                        'statusCode': 404,
+                        'body': json.dumps('not found')
+                    }
+            else: # cass == '{cmd}' 특정 레코드
+                cur.execute(f"SELECT {case} FROM bot WHERE bot_number = %s", (botNum))
+                result = cur.fetchone()
+                logger.info(result)
+
+                if result:
+                    logger.info("가져오기 완료")
+                    data = {
+                        'statusCode': 200,
+                        case: result[0]
+                    }
+                else:
+                    logger.error("해당 bot_number에 대한 레코드가 없습니다.")
+                    return {
+                        'statusCode': 404,
+                        'body': json.dumps('not found')
+                    }
+        return data
     except Exception as e:
         logger.error("오류 발생!")
         logger.error(e)
